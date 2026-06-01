ba165369d74537ae0f96b4ce0a032b074f2e2d54
usuario <barbyjr1215tiki@gmail.comm>
2026-06-01 03:59:52 -0600

    fix db config

--- config/db.config.js
+++ config/db.config.js
@@ -1,39 +1,24 @@
 const env = require('./env.js');
 const Sequelize = require('sequelize');
-
-class Database {
-  constructor() {
-    this.sequelize = new Sequelize(env.database, env.username, env.password, {
-      host: env.host,
-      dialect: env.dialect,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      },
-      pool: {
-        max: env.pool.max,      
-        min: env.pool.min,
-        acquire: env.pool.acquire,
-        idle: env.pool.idle
-      }
-    });
-
-    
-    this.Sequelize = Sequelize;
-
-    
-    this.initModels();
+const sequelize = new Sequelize(env.database, env.username, env.password, {
+  host: env.host,
+  dialect: env.dialect,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    max: env.pool.max,
+    min: env.pool.min,
+    acquire: env.pool.acquire,
+    idle: env.pool.idle,
   }
+});
 
-  initModels() {
-    
-    this.Juego = require('../models/estudiantes.model.js')(this.sequelize, this.Sequelize);
-  }
-}
-
-
-module.exports = new Database();
-
+const db = {};
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 db.Estudiante = require('../models/estudiante.model.js')(sequelize, Sequelize);
+module.exports = db;
